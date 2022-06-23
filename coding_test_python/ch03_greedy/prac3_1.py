cd6254b92a7e9fdb2e179e055c69227dd68c2158
Jiyong Oh <din2346@gmail.com>
2022-06-24 03:40:34 +0900

    first commit

--- coding_test_python/ch03_greedy/prac3_1.py
+++ coding_test_python/ch03_greedy/prac3_1.py
@@ -1,17 +1,18 @@
-# The law of large numbers is the law of making the largest number by adding a given number M times when there is an array of various numbers.
-# However, the number corresponding to a specific index of the array cannot be added more than K consecutively.
-# It is also considered to be different even when the number corresponding to different indexes is the same.
-# Output the result according to the law of large numbers given the size of the array N, the number of times the number is added M, and K.
+# '큰 수의 법칙'은 다양한 수로 이루어진 배열이 있을 대 주어진 수들을 M번 더하여 가장 큰 수를 만드는 법칙이다.
+# 단, 배열의 특정한 인덱스(번호)에 해당하는 수가 연속해서 K번을 초과하여 더해질 수 없는 것이 이 법칙의 특징이다.
+# 배열의 크기 N, 숫자가 더해지는 횟수 M, 그리고 K가 주어질 때 큰 수의 법칙에 따른 결과를 출력하시오.
 
 N, M, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
+# 리스트를 정렬한다.(오름차순이 default)
 arr.sort()
 first = arr[N - 1]
 second = arr[N - 2]
 
 sum = 0
 
+# 루프를 돌며 '가장 큰 수를 K번 더하고 두 번째로 큰 수를 한 번 더하는 연산'을 반복한다.
 while True:
     for i in range(K):
         if M == 0:
@@ -26,3 +27,15 @@ while True:
     M -= 1
 
 print(sum)
+
+# 첫 번째 version의 코드는 O(M)의 시간 복잡도를 가진다.
+# 계산을 통해 아래와 같이 O(1)의 시간 복잡도를 갖도록 문제를 풀 수 있다.
+
+first = arr[N - 1]
+second = arr[N - 2]
+
+in_a_row = first * K + second
+
+result = in_a_row * (M // (K + 1)) + first * (M % (K + 1))
+
+print(result)
--- coding_test_python/ch03_greedy/prac3_2.py
+++ coding_test_python/ch03_greedy/prac3_2.py
@@ -1,11 +1,10 @@
-# Number card game is a game in which one card with the highest number is drawn from among several number cards.
-# However, you must draw cards while observing the rules of the game, and the rules are as follows.
-# 1. Numbered cards are placed in the shape of N x M.
-#    In this case, N means the number of rows and M means the number of columns.
-# 2. First, select the row containing the card you want to draw.
-# 3. Then, you must draw the lowest numbered card among the cards in the selected row.
-# 4. Therefore, when selecting a row from which to draw cards at the beginning, you should plan a strategy so that you can draw the highest number of cards in the end, considering that the lowest card will be drawn from that row later.
-# Create a program that draws cards according to the rules of the game when the cards are placed in the shape of N x M.
+# 숫자 카드 게임은 여러 개의 숫자 카드 중에서 가장 높은 숫자가 쓰인 카드 한 장을 뽑는 게임이다.
+# 단, 게임의 룰을 지키며 카드를 봅아야 하고 룰은 다음과 같다.
+# 1. 숫자가 쓰인 카드들이 N x M 형태로 놓여 있다. 이때 N은 행의 개수를 의미하며, M은 열의 개수를 의미한다.
+# 2. 먼저 뽑고자 하는 카드가 포함되어 있는 행을 선택한다.
+# 3. 그 다음 선택된 행에 포함된 카드들 중 가장 숫자가 낮은 카드를 뽑아야 한다.
+# 4. 따라서 처음에 카드를 골라낼 행을 선택할 때, 이후에 해당 행에서 가장 숫자가 낮은 카드를 뽑을 것을 고려하여
+#    최종적으로 가장 높은 숫자의 카드를 뽑을 수 있도록 전략을 세워야 한다.
 
 n, m = map(int, input().split())
 min_cards = []
